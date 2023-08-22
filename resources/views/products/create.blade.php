6cc1a342697acb75e0b11b7d7f5e64b230b63680
Mohamad12 <mohamadkhaled.aub@gmail.com>
2023-08-22 16:45:20 +0300

    design update

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight text-center">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight ">
             {{ __('Products') }}
         </h2>
     </x-slot>
