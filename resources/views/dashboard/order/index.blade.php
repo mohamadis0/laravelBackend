2eb5e826713d38fa8ea8c4316635bbdbe07d5b59
mohamadis0 <124500678+mohamadis0@users.noreply.github.com>
2023-08-22 16:13:34 +0300

    after conflicts

--- resources/views/dashboard/order/index.blade.php
+++ resources/views/dashboard/order/index.blade.php
@@ -1,59 +1,16 @@
-{{-- <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container">
-        <div>
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">By</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Created Date</th>
-                    <th scope="col">Ordered Date</th>
-                    <th scope="col" class="text-center">Actions</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach ($orders as $order)
-                        <tr>
-                            <td scope="row">{{$order->id}}</td>
-                            <td>{{$order->client->user->name}}</td>
-                            <td>{{$order->status}}</td>
-                            <td>{{$order->created_at}}</td>
-                            <td>{{$order->ordered_date}}</td>
-                            <td class="text-center">
-                                <a href="{{ route('order.show', $order->id) }}" class="btn btn-primary">More Info</a>
 
-                                <button class="btn btn-danger">Delete</button>
-                            </td>
-                        </tr>
-                    @endforeach
-                  
-                </tbody>
-              </table>
-        </div>
-        <div>
-        </div>
-        
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  </body>
-</html> --}}
 <x-app-layout>
+  <x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Order') }}
+    </h2>
+</x-slot>
   <div class="container">
     <div class="flex flex-col">
       <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+        <div class="inline-block w-full py-2 sm:px-6 lg:px-8">
           <div class="overflow-hidden">
-              <table class="min-w-full text-left text-sm font-light">
+              <table class="bg-white w-full text-left text-sm font-light">
                 <thead class="border-b font-medium dark:border-neutral-500">
                   
                   <tr>
