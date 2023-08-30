64de62246bb707173913fb06c32e61a0c9502320
oussama <oussamanoeman4@gmail.com>
2023-08-30 15:33:10 +0300

    oussama

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -235,7 +235,7 @@ class OrderController extends Controller
                 $orderlines = OrderLine::where('order_id',$order->id)->get();
                 if($orderlines->isEmpty())
                 {
-                    OrderDetails::where('order_id')->delete();
+                    OrderDetails::where('order_id',$order->id)->delete();
                     $order->delete();
                 }
                 return response()->json([
