81c0478315f4a819b7cab5aca64aa1410252646b
mohamadis0 <124500678+mohamadis0@users.noreply.github.com>
2023-09-01 12:08:57 +0300

    push

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
     public function index()
     {
 
-        $products=Product::all()->where('feature','product');
+        $products=Product::where('feature','product')->get();
         return response()->json([
             'success'=>true,
             'message'=>'all products',
